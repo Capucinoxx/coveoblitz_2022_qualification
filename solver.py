113983fe7aebf837414bc0f8e0f7b58c4a792662
Capucinoxx <bendertermebc@gmail.com>
2021-11-14 20:50:29 -0500

    toute refaire le code pour ne pas que ce;la prenne 8 ans et demi

--- solver.py
+++ solver.py
@@ -2,55 +2,11 @@ from game_interface import Answer, GameMessage, TotemAnswer
 
 # objet magique de debug qui compte le nombre de fois que chaque fonction est call
 from time import perf_counter
-from collections import defaultdict
-
-
-class FuncMarker:
-
-    def __init__(self):
-        self.call_count = 0
-        self.cum = 0
-
-    def __add__(self, time):
-        self.cum += perf_counter() - time
-        self.call_count += 1
-        return self
-
-    def __str__(self):
-        return f"{self.cum:,.5f}s [{self.call_count:,}]"
-
-
-class TimeIt:
-    stats = defaultdict(FuncMarker)
-
-    def __call__(self, func):
-        def wrapped(*args, **kwargs):
-            start = perf_counter()
-            r = func(*args, **kwargs)
-            self.stats[func.__name__] += start
-            return r
-
-        return wrapped
-
-    def report(self):
-        for k, v in self.stats.items():
-            print(k, v)
-
-    def __del__(self):
-        self.report()
-
-
-timeit = TimeIt()
-
-
-########################################################################################################################
 
 class Solver:
     def __init__(self):
         self.Board = None
-        pass
 
-    @timeit
     def get_answer(self, game_message: GameMessage) -> Answer:
         # on récupère la question
         question = game_message.payload
@@ -59,16 +15,17 @@ class Solver:
         start = perf_counter()
         answer = Answer(Board(question.totems).set_totems())
         end = perf_counter()
+        print("took: ", (end - start), " \n\n\n")
         # on crée un noveau board
         print("Sending Answer", ":", answer, "\n\t length = (", len(answer.totems), ")")
 
-        print("took: ", (end - start), " \n\n\n")
         return answer
 
 
 class Board:
     def __init__(self, payload):
         self.board = {}
+        self.test_position = {(0, 0): True}
 
         self.shapes = {}
         self.length = len(payload)
@@ -114,11 +71,9 @@ class Board:
             ]
         }
 
-    @timeit
     def totem_answer(self, name, coordinates):
         return TotemAnswer(shape=name, coordinates=coordinates)
 
-    @timeit
     def get_new_size(self, coords):
         w = self.width
         h = self.height
@@ -129,7 +84,6 @@ class Board:
                 h = y
         return w, h
 
-    @timeit
     def count_shape(self, payload):
         """
     compte le nombres de fois que la pièce apparait dans le payload
@@ -138,22 +92,18 @@ class Board:
         while payload:
             self.shapes[payload.pop().shape] += 1
 
-    @timeit
     def context_totem_coords(self, totem_coords, x, y):
         return [(_x + x, _y + y) for _x, _y in totem_coords]
 
-    @timeit
     def get_shape(self, totem):
         return self.totems[totem]
 
-    @timeit
     def position_is_valid(self, coords):
         for coord in coords:
             if coord in self.board:
                 return False
         return True
 
-    @timeit
     def add_shape_to_board(self, coords):
         for x, y in coords:
             self.board[(x, y)] = True
@@ -162,73 +112,81 @@ class Board:
             if y > self.height:
                 self.height = y
 
-    # TODO: la FONCTION DE MERDE que l'on doit retaper, elle est 5 fois trop lente !
-    # TODO: remplacer calcul ratio par un calcul du pointage, car le calcul raito cause trop d'erreur
-    @timeit
-    def set_totems(self):
-        answer = []
-
-        for i in range(0, self.length):
-            choice = self.calculate_best_totem()
-            if choice is not None:
-                name, coord = choice
-                self.shapes[name] -= 1
-                self.add_shape_to_board(coord)
-                answer.append(self.totem_answer(name, coord))
-                self.nb_totem_placed += 1
-        x = self.width
-        y = self.height
-        print("score: ", (self.length * 10 - ((x + 1) * (y + 1))) * (min(x+1, y+1) / max(x+1, y+1)))
-        return answer
-
-    @timeit
     def calculate_best_totem(self):
         choice = None
         best_score = 0
 
         for totem, count in self.shapes.items():
-            x = y = 0
             if count > 0:
-                if self.width + self.height == 0:
-                    return totem, self.get_shape(totem)[0]
-
-                current_best, current_best_score = self.get_best_rotation(totem)
+                current_best, current_best_score = self.get_totem_best_rotation(totem)
 
                 if current_best_score > best_score:
                     choice = current_best
                     best_score = current_best_score
-
         return choice
 
-    @timeit
-    def get_best_rotation(self, totem):
-        x = y = 0
-
+    def get_totem_best_rotation(self, totem):
         current_best = None
-       # current_best_ratio = 0
         current_best_score = 0
-        is_fucking_done = False
-        while not is_fucking_done:
-            has_found = False
+        deleted_position = []
 
-            while not has_found:
-                for rotation in self.get_shape(totem):
-                    t = self.context_totem_coords(rotation, x, y)
-                    if self.position_is_valid(t):
-                        has_found = True
-                        nx, ny = self.get_new_size(t)
-                        #ratio = min(nx, ny) / max(nx, ny)
-                        score = self.calculate_score(nx, ny)
-                        if score > current_best_score:
-                            current_best_score = score
-                            current_best = (totem, t)
-                        if x == 0:
-                            is_fucking_done = True
-                x += 1
-            x = 0
-            y += 1
+        # pour chaque cellule vide
+        for x, y in self.test_position.keys():
+            is_impossible = True
+            # pour chaque rotation
+            for rotation in self.get_shape(totem):
+                t = self.context_totem_coords(rotation, x, y)  # on calcule les position de la pièce
+                if self.position_is_valid(t):
+                    is_impossible = False
+                    nx, ny = self.get_new_size(t)
+                    score = self.calculate_score(nx, ny)
+                    if score > current_best_score:
+                        current_best_score = score
+                        current_best = (totem, t)
+            if is_impossible:
+                deleted_position.append((x, y))
+
+        for pos in deleted_position:
+            self.test_position.pop(pos)
 
         return current_best, current_best_score
 
+    def set_totems(self):
+        answer = []
+        # pour chaque totem à placer
+        for i in range(0, self.length):
+            # on calcul le meilleur totem
+            choice = self.calculate_best_totem()
+
+            if choice is None:
+                self.test_position.update({(self.width + 1, i): True for i in range(self.height + 2)})
+                self.test_position.update({(i, self.height + 1): True for i in range(self.width + 2)})
+                choice = self.calculate_best_totem()
+
+            name, coords = choice
+            old_width = self.width
+            old_height = self.height
+            self.shapes[name] -= 1
+            self.add_shape_to_board(coords)
+            answer.append(self.totem_answer(name, coords))
+            self.nb_totem_placed += 1
+
+            # - on retire les coordonnées déjà utilisées
+            for coord in coords:
+                self.test_position.pop(coord, None)
+
+            if old_width != self.width:
+                self.test_position.update({(self.width, i): True for i in range(self.height + 1)})
+
+            if old_height != self.height:
+                self.test_position.update({(i, self.height): True for i in range(self.width + 1)})
+
+        x = self.width
+        y = self.height
+        print("score: ", (self.length * 10 - ((x + 1) * (y + 1))) * (min(x + 1, y + 1) / max(x + 1, y + 1)))
+
+        # on actualise les
+        return answer
+
     def calculate_score(self, x, y):
-        return ((self.nb_totem_placed + 1) * 10 - ((x + 1) * (y + 1))) * (min(x+1, y+1) / max(x+1, y+1))
+        return ((self.nb_totem_placed + 1) * 10 - ((x + 1) * (y + 1))) * (min(x + 1, y + 1) / max(x + 1, y + 1))
